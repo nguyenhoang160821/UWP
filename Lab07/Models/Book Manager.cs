cf28cae52a50dff11c31355e038ae73a5cce0fac
nguyenhoang160821 <54625158+nguyenhoang160821@users.noreply.github.com>
2020-09-09 15:35:50 +0700

    Lab 07

---
+++ Lab07/Models/Book Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Models
+{
+    class Book_Manager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = " Vulpate", Author = "Futurun", CoverImage = "Assets/1.png" });
+            books.Add(new Book { BookId = 2, Title = " Mazin", Author = "Sequiter Que", CoverImage = "Assets/2.png" });
+            books.Add(new Book { BookId = 3, Title = " Elit", Author = "Tempor", CoverImage = "Assets/3.png" });
+            return books;
+        }
+    }
+}
