50ff4916646a1e9434021cb53751137251f4c70f
nguyenhoang160821 <54625158+nguyenhoang160821@users.noreply.github.com>
2020-09-15 14:59:35 +0700

    Lab 09

---
+++ Lab 09/Model/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_09.Model
+{
+    public class MenuItem
+    {
+        public String IconFile { get; set; }
+        public SoundCategory Category { get; set; }
+    }
+}
---
+++ Lab 09/Model/Sound.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Audio;
+
+namespace Lab_09.Model
+{
+    public class Sound
+    {
+        public String Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public String AudioFile { get; set; }
+        public String ImageFile { get; set; }
+
+        public Sound(String name, SoundCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = String.Format("/Assets/Audio/{0}/{1}.wav", category, name);
+            ImageFile = String.Format("/Assets/Audio/{0}/{1}.png", category, name);
+        }
+
+
+    }
+
+
+    public enum SoundCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+    }
+
+
+}
