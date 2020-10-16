b0587605b9f287c9b56d66dd915385de61fc3700
nguyenhoang160821 <54625158+nguyenhoang160821@users.noreply.github.com>
2020-10-16 11:31:02 +0700

    Baithi -3

---
+++ Practice2/Practice2/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pracetice2.Models;
+using Pracetice2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pracetice2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Products> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
---
+++ Practice2/Practice2/Models/products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Pracetice2.Models
+{
+    public class Products
+    {
+        public string ID { get; set; }
+
+        public string name { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string Price { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Products>(this);
+    }
+}
